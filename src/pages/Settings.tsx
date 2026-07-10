05da51e14d45a5d29f5e86536c137fa5ecf5294c
Josias Lima <josiaslcneto@gmail.com>
2026-07-10 15:16:21 -0300

    Ajuste visual/3

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -25,11 +25,11 @@ export function Settings() {
     <div className="w-full max-w-[1200px] mx-auto">
         <SimpleHeader title="Configurações" subtitle="Gerenciar configurações da página"/>
         <p>Trocar tema da página</p>
-        <div className="flex flex-col gap-4 mt-4">
+        <div className="flex flex-col gap-4 mt-4 w-fit">
             <button onClick={toggleTheme} className="btn btn-primary rounded-lg">
                 {theme === "forest" ? "☀️ Light Mode": "🌙 Dark Mode"}
             </button>
-            <button onClick={handleLogout} className="btn btn-error mt-6">
+            <button onClick={handleLogout} className="btn btn-error rounded-lg mt-6">
                 Logout
             </button>
         </div>
