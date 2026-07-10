dbd5124e9849ed8f5261eef6058b37104be42ea4
Josias Lima <josiaslcneto@gmail.com>
2026-07-10 14:57:43 -0300

    Adicionado troca de tema

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,9 +1,21 @@
 import { useNavigate } from 'react-router-dom';
 import { clearToken } from '@/lib/api';
+import { useEffect, useState } from "react";
 
 export function Settings() {
     const navigate = useNavigate();
 
+    const [theme, setTheme] = useState(
+    localStorage.getItem("theme") || "forest"
+);
+
+    useEffect(() => {
+        document.documentElement.setAttribute("data-theme", theme);
+        localStorage.setItem("theme", theme);
+    }, [theme]);
+    function toggleTheme() {
+        setTheme(theme === "forest" ? "light" : "forest");
+    }
     function handleLogout() {
     clearToken();
     navigate("/login");
@@ -11,9 +23,11 @@ export function Settings() {
     return (
     <div className="p-8">
         <h1 className="text-3xl font-bold">Configurações</h1>
-        <p className="mt-4">Página em desenvolvimento.</p>
+        <button onClick={toggleTheme} className="btn btn-primary">
+            {theme === "forest" ? "☀️ Light Mode": "🌙 Forest Mode"}
+        </button>
         <button onClick={handleLogout} className="btn btn-error mt-6">
-        Logout
+            Logout
         </button>
     </div>
     );
